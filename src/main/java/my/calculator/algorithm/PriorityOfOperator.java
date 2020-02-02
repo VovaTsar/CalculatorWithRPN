c1eef8eae73fc8baf509e1838f206089fae19a69
VovaTsar <dropttecttt@mail.ru>
2020-02-03 00:27:41 +0200

    Fix PriorityOfOperator

--- src/main/java/my/calculator/algorithm/PriorityOfOperator.java
+++ src/main/java/my/calculator/algorithm/PriorityOfOperator.java
@@ -2,22 +2,28 @@ package my.calculator.algorithm;
 
 public class PriorityOfOperator {
 
+    private static final int HIGHER_PRIORITY = 3;
+    private static final int LESS_PRIORITY = 2;
+    private static final int OPENING_LABEL = 1;
+    private static final int CLOSING_LABEL = -1;
+    private static final int NUMBER = 0;
+
     public int getPriority(char operator) {
         switch (operator) {
             case '*':
-                return 3;
+                return HIGHER_PRIORITY;
             case '/':
-                return 3;
+                return HIGHER_PRIORITY;
             case '+':
-                return 2;
+                return LESS_PRIORITY;
             case '-':
-                return 2;
+                return LESS_PRIORITY;
             case '(':
-                return 1;
+                return OPENING_LABEL;
             case ')':
-                return -1;
+                return CLOSING_LABEL;
             default:
-                return 0;
+                return NUMBER;
         }
     }
 }
